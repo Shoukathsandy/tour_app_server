5c427bbf19e1c4ca3b1368d55c2e0e767903a4e2
Shoukathsandy <shoukathsandy@gmail.com>
2022-10-21 18:07:31 +0530

    ignore

--- index.js
+++ index.js
@@ -13,6 +13,9 @@ app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
+app.get("/",function(req,res){
+  res.send("this is tour app server");
+})
 app.use("/users", userRouter); // http://localhost:5000/users/signup
 app.use("/tour", tourRouter);
 
